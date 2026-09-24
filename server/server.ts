c18338183dbd521455d131b202a12be81bc2636a
Xorse Wisdom <wisdomxorse928@gmail.com>
2026-09-24 13:03:10 +0000

    minor 2

--- server/server.ts
+++ server/server.ts
@@ -17,10 +17,11 @@ import uploadRouter from "./routes/uploadRoutes.js";
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "https://nunya-iota.vercel.app"],
-    credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: ["http://localhost:5173", "http://localhost:3000", "https://nunya-iota.vercel.app"],
+//     credentials: true
+// }));
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
