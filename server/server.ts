8501bd833ddbca655f72a940c5a7d13321ab7baa
Xorse Wisdom <wisdomxorse928@gmail.com>
2026-09-24 12:49:40 +0000

    minor commit

--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000", process.env.VITE_BASE_URL|| "*"],
     credentials: true
 }));
 app.use(express.json());
